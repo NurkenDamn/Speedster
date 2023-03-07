b9c8f9fdaa5f1c1b801c72cfd1be1e7d37ba5ca0
Кенес Нуркен Абилхайырулы <@lcoast.itstep>
2023-03-07 17:48:42 +0600

    add logo file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
 
 import App from './components/App/App'
 
@@ -8,8 +7,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider >
-        <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
 )
