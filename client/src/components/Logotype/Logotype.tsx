c8f82b95c8217a7297ae95211312a77cfc03681a
Кенес Нуркен Абилхайырулы <@lcoast.itstep>
2023-03-07 17:49:18 +0600

    add Logotype component

---
+++ client/src/components/Logotype/Logotype.tsx
@@ -0,0 +1,16 @@
+import {
+  Link,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react'
+
+const Logotype = () => {
+
+  return (
+    <>
+      {/* TODO logotype */}
+    </>
+  )
+}
+
+export default Logotype
