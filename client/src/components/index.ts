c5a7f2500b0682dbfdd67280b5aac15b7ec4e5cf
unknown <nurkenkenes7@gmail.com>
2023-03-08 18:16:50 +0600

    add index file to component folder

---
+++ client/src/components/index.ts
@@ -0,0 +1,13 @@
+import Navbar from './Navbar/Navbar'
+import Logotype from './Logotype/Logotype'
+import LinkItem from './LinkItem/LinkItem'
+import ThemeToggleButton from './ThemeToggleButton/ThemeToggleButton'
+import CustomMenu from './CustomMenu/CustomMenu'
+
+export {
+  Navbar,
+  Logotype,
+  LinkItem,
+  ThemeToggleButton,
+  CustomMenu
+}
